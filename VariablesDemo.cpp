34b663ea9d0a344c8195c5047f39cc6fc75523ea
Erika Sanders <erikalsanders@msn.com>
2026-02-04 15:41:34 -0700

    Add VariablesDemo.cpp to display variable values
    This program defines and displays a year, temperature, and initial.

---
+++ VariablesDemo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable definitions
+    int year = 2025;
+    double temperature = 98.6;
+    char initial = 'P';
+
+    // Displaying values
+    cout << "Year: " << year << endl;
+    cout << "Temperature: " << temperature << endl;
+    cout << "Initial: " << initial << endl;
+
+    return 0;
+}
