71b83e4bd58a9be92568691560284eb3e60549a5
Erika Sanders <erikalsanders@msn.com>
2026-02-04 15:35:42 -0700

    Add StudentInfo program to display student bio
    This program displays a brief student bio using cout statements, comments, and escape sequences.

---
+++ Basics/StudentInfo.cpp
@@ -0,0 +1,30 @@
+/*
+----------------------------------------------------
+Student Name:   Erika Sanders
+Course#:        CS101 - Introduction to Programming
+Date:           January 28, 2026
+Program Purpose:
+This program displays a brief student bio using
+cout statements, comments, and escape sequences.
+----------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Display a formatted title using escape sequences
+    cout << "=================================\n";
+    cout << "\t\tSTUDENT INFO\n";
+    cout << "=================================\n\n";
+
+    // Student introduction
+    cout << "Hello! My name is Erika Sanders.\n";
+    cout << "Major:\t\tGame Development & Design\n";
+    cout << "Hobby:\t\t\"Creating games and exploring AR/VR\"\n";
+    cout << "Goal:\t\tTo build immersive educational games\n";
+
+    // End of program
+    return 0;
+}
